6505da326aec4a8163d941073f5d818be3052500
GreyWindler <43067710+GreyWindler@users.noreply.github.com>
2018-09-23 21:41:50 +0300

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,9 +1,13 @@
+#!/usr/bin/python
 import requests
+from argparse import ArgumentParser
 
-def translate_it(text):
+API_KEY = 'trnsl.1.1.20180921T202554Z.2f0ebf91fae2d8e4.9ce767aec09c8fc6dbf3fcae566a21c15408f8ac'
+URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+
+
+def translate_it(text, from_lang, to_lang):
     """
-    YANDEX translation plugin
-    docs: https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
     https://translate.yandex.net/api/v1.5/tr.json/translate ?
     key=<API-ключ>
      & text=<переводимый текст>
@@ -11,19 +15,34 @@ def translate_it(text):
      & [format=<формат текста>]
      & [options=<опции перевода>]
      & [callback=<имя callback-функции>]
-    :param text: <str> text for translation.
-    :return: <str> translated text.
+
+    :param to_lang:
+    :return:
     """
-    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-    key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
 
     params = {
-        'key': key,
-        'lang': 'ru-en',
+        'key': API_KEY,
         'text': text,
+        'lang': '%s-%s' % (from_lang, to_lang),
     }
-    response = requests.get(url, params=params).json()
-    return ' '.join(response.get('text', []))
+    response = requests.get(URL, params=params)
+    json_ = response.json()
+    return ''.join(json_['text'])
 
-a = translate_it('Привет')
-print(a)
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('--lang-to', dest='langto')
+    parser.add_argument('--lang-from', dest='langfrom', required=True)
+    parser.add_argument('--path-to', dest='pathto', required=True)
+    parser.add_argument('--path-from', dest='pathfrom', required=True)
+
+    args = parser.parse_args()
+    langto = 'ru'
+    if args.langto:
+        langto = args.langto
+    with open(args.pathfrom, 'r') as pf:
+        with open(args.pathto, 'w') as pt:
+            text = pf.read()
+            res = translate_it(text, args.langfrom, langto)
+            pt.write(res)
